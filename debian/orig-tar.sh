e2a35f6b80fb6f3b99ede834502c7f5330220118
Sylvestre Ledru <sylvestre@debian.org>
2025-07-28 21:02:48 +0200

    improve the repack  script

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -31,11 +31,11 @@ reset_repo ()
     cd $1
     git clean -qfd
     git checkout .
-    git remote update > /dev/null
+    git remote update origin > /dev/null
     git reset --hard origin/main > /dev/null
     git clean -qfd
     git checkout main > /dev/null
-    git pull
+    git pull origin main
     cd -
 }
 
@@ -113,6 +113,8 @@ fi
 cd $EXPORT_PATH/llvm-project
 if test -z  "$TAG" -a -z "$FINAL_RELEASE"; then
     # Building a branch
+    # Fetch the specific branch from origin to avoid ambiguity
+    git fetch origin $BRANCH:$BRANCH
     git checkout $BRANCH
     git reset --hard origin/$BRANCH
     if test $BRANCH != "main"; then
